4d505bc7904438e7bf043816831dfeb1693eb308
Jeff Reyes <61217933+jeffreyes900@users.noreply.github.com>
2020-06-09 16:00:32 -0700

    summary (required)

---
+++ 12_Final/js/myScripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+    // jQuery methods go here...
+
+    $('.main-carousel').flickity({
+        cellAlign: 'left',
+        contain: true,
+        freeScroll: true,
+        wrapAround: true,
+        watchCSS: true
+    });
+
+});
