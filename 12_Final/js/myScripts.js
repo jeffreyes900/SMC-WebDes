4fea82fd0835d7d6b516df42f8697e777d8b1a5c
Jeff Reyes <61217933+jeffreyes900@users.noreply.github.com>
2020-06-16 17:48:13 -0700

    Summary (required)

--- 12_Final/js/myScripts.js
+++ 12_Final/js/myScripts.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
 
     // jQuery methods go here...
 
+
+
     $('.main-carousel').flickity({
         cellAlign: 'left',
         contain: true,
@@ -11,43 +13,6 @@ $(document).ready(function() {
         prevNextButtons: false,
         pageDots: false
     });
-
-
-
-    var blogButton1 = document.getElementById("b1");
-    var blogButton2 = document.getElementById("b2");
-    var blogButton3 = document.getElementById("b3");
-
-    var toggleArticleVisibility = function(button) {
-        console.log(blogButton.innerText);
-
-    }
-
-    blogButton1.addEventListener("click", toggleArticleVisibility('b1'));
-    blogButton2.addEventListener("click", toggleArticleVisibility('b2'));
-    blogButton3.addEventListener("click", toggleArticleVisibility('b3'));
-
-    // 2. This code loads the IFrame Player API code asynchronously.
-    var tag = document.createElement('script');
-
-    tag.src = "https://www.youtube.com/iframe_api";
-    var firstScriptTag = document.getElementsByTagName('script')[0];
-    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-    // 3. This function creates an <iframe> (and YouTube player)
-    //    after the API code downloads.
-    var player;
-
-    function onYouTubeIframeAPIReady() {
-        player = new YT.Player('player', {
-            videoId: '2H7faKIcdHs',
-            events: {
-                'onReady': onPlayerReady,
-                'onStateChange': onPlayerStateChange
-            }
-        });
-    }
-
 
 
 });
